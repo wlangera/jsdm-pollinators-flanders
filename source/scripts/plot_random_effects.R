6d38a264dc6729cdea7ec6e3dbc01d0ea9ab7ece
Ward Langeraert <ward.langeraert@inbo.be>
2025-07-28 11:56:07 +0200

    check coding style

--- source/scripts/plot_random_effects.R
+++ source/scripts/plot_random_effects.R
@@ -1,24 +1,32 @@
 plot_random_effects <- function(gllvm_model) {
+  require("ggplot2")
+  require("dplyr")
+  require("rlang")
+
   estimates <- gllvm_model$params$Br[-1, ] %>%
     as_tibble(rownames = "param") %>%
-    pivot_longer(-param, names_to = "species", values_to = "estimate")
+    pivot_longer(-"param", names_to = "species", values_to = "estimate")
 
   ses_gllvm_model <- gllvm::getPredictErr(gllvm_model)$Br[-1, ] %>%
     as_tibble(rownames = "param") %>%
-    pivot_longer(-param, names_to = "species", values_to = "se")
+    pivot_longer(-"param", names_to = "species", values_to = "se")
 
-  full_join(estimates, ses_gllvm_model, by = join_by(param, species)) %>%
-    mutate(order = mean(estimate), .by = "species") %>%
+  full_join(estimates, ses_gllvm_model,
+            by = join_by("param", "species")) %>%
+    mutate(order = mean(.data$estimate), .by = "species") %>%
     mutate(
-      lcl = estimate + qnorm(0.025) * se,
-      ucl = estimate + qnorm(0.975) * se,
-      significant = !(0 > lcl & 0 < ucl),
-      species = reorder(species, order)
+      lcl = .data$estimate + qnorm(0.025) * .data$se,
+      ucl = .data$estimate + qnorm(0.975) * .data$se,
+      significant = !(0 > .data$lcl & 0 < .data$ucl),
+      species = reorder(.data$species, order)
     ) %>%
-    ggplot(aes(x = estimate, y = species, colour = param, alpha =  significant)) +
+    ggplot(
+      aes(x = .data$estimate, y = .data$species,
+          colour = .data$param, alpha =  .data$significant)
+    ) +
     geom_vline(xintercept = 0, linetype = "dashed", colour = "darkgrey") +
     geom_point(position = position_dodge(width = 0.8)) +
-    geom_errorbar(aes(xmin = lcl, xmax = ucl),
+    geom_errorbar(aes(xmin = .data$lcl, xmax = .data$ucl),
                   position = position_dodge(width = 0.8)) +
     labs(x = "compared to dry heather", y = "", colour = "Legend:") +
     theme_minimal() +
--- source/scripts/plot_variance_partitioning.R
+++ source/scripts/plot_variance_partitioning.R
@@ -1,24 +1,30 @@
 plot_variance_partitioning <- function(gllvm_model) {
+  require("ggplot2")
+  require("dplyr")
+  require("rlang")
+
   var_part <- varPartitioning(gllvm_model)
   var_part$PropExplainedVarSp %>%
     as_tibble(rownames = "species") %>%
-    pivot_longer(-species, names_to = "part", values_to = "percentage") %>%
+    pivot_longer(-"species", names_to = "part", values_to = "percentage") %>%
     mutate(
       Legend = factor(
         paste0(
-          gsub("/", "\n", part),
+          gsub("/", "\n", .data$part),
           "\n-->mean: ",
           round(colMeans(var_part$PropExplainedVarSp), 3)
         ),
       )
     ) %>%
-    ggplot(aes(x = species, y = percentage, fill = Legend)) +
+    ggplot(aes(x = .data$species, y = .data$percentage, fill = .data$Legend)) +
     geom_bar(stat = "identity") +
     labs(x = "", y = "Variance proportion") +
     theme_minimal() +
     theme(
       legend.position = "bottom",
       legend.text = element_text(size = 8),
-      axis.text.x = element_text(size = 7, angle = 90, hjust = 0.95, vjust = 0.2)
+      axis.text.x = element_text(
+        size = 7, angle = 90, hjust = 0.95, vjust = 0.2
+      )
     )
 }
